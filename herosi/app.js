d23a26ec2e096a70ef269044f3b378f0ff217d37
Dorota Pawłowska <dpawlowska@op.pl>
2018-04-07 15:45:59 +0200

    herosi - zajecia 7.04.2018 part1

--- herosi/app.js
+++ herosi/app.js
@@ -58,3 +58,43 @@ heroesList.forEach(function (hero) {
     heroesListRegion.appendChild(element);
 });
 
+var addHeroButton = document.getElementById('addHero');
+
+addHeroButton.addEventListener('click', function (event) {
+    event.preventDefault();
+
+
+
+    var addHeroRegion = document.getElementById('addHero-region'),
+        addHeroTemplate = ''
+            + '<div class="form-container">'
+            + ' <form>'
+            + '     <h1>Dodaj Herosa</h1>'
+            + '     <div class="form-group">'
+            + '         <input type="text" name="name" />'
+            + '         <label class="control-label" for="name">Nazwa Bohatera</label><i class="bar"></i>'
+            + '     </div>'
+            + '     <div class="form-group">'
+            + '         <input type="text" name="photo" />'
+            + '         <label class="control-label" for="photo">Adres/nazwa zdjęcia</label><i class="bar"></i>'
+            + '     </div>'
+            + '     <div class="form-group">'
+            + '         <input type="text" name="price" />'
+            + '         <label class="control-label" for="price">Cena wynajmu /h</label><i class="bar"></i>'
+            + '     </div>'
+            + '     <div class="form-group">'
+            + '         <textarea></textarea>'
+            + '         <label class="control-label" for="textarea">Opis Bohatera</label><i class="bar"></i>'
+            + '     </div>'
+            + ' </form>'
+            + ' <div class="button-container">'
+            + '     <button class="button" type="button"><span>Submit</span></button>'
+            + ' </div>'
+            + '</div>';
+
+    var HTMLelement = document.createElement('div');
+
+    HTMLelement.innerHTML = addHeroTemplate;
+    addHeroRegion.appendChild(HTMLelement);
+
+})
