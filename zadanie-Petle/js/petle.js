a6589d81d659078745af9e4319fbb3b96665e40b
Dorota Pawłowska <dpawlowska@op.pl>
2018-05-17 19:23:13 +0200

    materiały do zadań z pętlami z JS

---
+++ zadanie-Petle/js/petle.js
@@ -0,0 +1,149 @@
+/*
+*
+*     _____                     _  __                               _ 
+*    / ____|                   | |/ /                              (_)
+*   | |     _______  ___  ___  | ' /_   _ _ __ ___  __ _ _ __   ___ _ 
+*   | |    |_  / _ \/ __|/ __| |  <| | | | '__/ __|/ _` | '_ \ / __| |
+*   | |____ / /  __/\__ \ (__  | . \ |_| | |  \__ \ (_| | | | | (__| |
+*    \_____/___\___||___/\___| |_|\_\__,_|_|  |___/\__,_|_| |_|\___|_|
+*                                                                     
+*   Po tych zadaniach będizecie już mistrzami pętli w JS !
+*
+*/
+
+
+// 1.
+// Stwórz tablicę złożoną z 10 elementów i wyświetl je w konsoli po kolei
+// 
+
+var show = function(){
+  // TODO
+}
+// show();
+
+// 2.
+// Wyświetl w konsoli liczby parzyste od 0 do 100
+// 
+
+var even = function () {
+  // TODO
+}
+// even();
+
+
+// 3.
+// Wyświetl w konsoli liczby od 100 do 1
+// 
+
+var hundred = function(){
+  // TODO
+}
+// hundred();
+
+// 4.
+// Stwórz tablicę, a następnie 
+// przy pomocą pętli stwórz tablicę zawierającą 
+// te same elementy w odwrotnej kolejności. 
+// Wyświetl je w alercie. 
+// 
+
+var reverseArray = function(){
+  // TODO
+}
+
+
+// 5.
+// ** Za pomocą pętli policz silnię z dowolnej, 
+// podanej liczby 
+//
+
+var factorial = function(number){
+  // TODO
+}
+
+// 6.
+// Stwórz obiekt i wyświetl na stronie wszystkie jego elementy 
+// w formacie: "(klucz) wynosi (wartość)". 
+// Funkcji nie wywołuj w konsoli
+//
+
+var showObject = function(){
+  // TODO
+}
+
+// 7.
+// stwórz tablicę zawierającą 3 obiekty. 
+// Wyświetl na stronie wszystkie elementy 
+// wszystkich obiektów, zaznaczając którego obiektu 
+// elementy są wyświetlone. 
+// Funkcji nie wywołuj w konsoli
+//
+
+var showObjectArray = function(){
+  // TODO
+}
+
+// 8.
+// Stwórz tablicę złożoną z 10 kolejnych wielokrotności 
+// liczby 5 za pomocą pętli while
+// 
+
+var multipleOf5 = function(){
+  // TODO
+}
+
+// 9.
+// Wyświetl w konsoli liczby parzyste od 0 do 100 
+// za pomocą pętli while
+// 
+
+var evenNumbers = function(){
+  // TODO
+}
+
+// 10.
+// Pobieraj od użytkownika liczby tak długo, 
+// aż wpiszę liczbę większą niż 50. 
+// Wtedy wyświetl tablicę złożoną z wpisanych 
+// dotychczas liczb na stronie
+//
+
+var arrayLessThen50 = function(){    
+  // TODO
+}
+
+// 11.
+// Wypisz na stronie elementy poniższej tablicy 
+// do elementu "stop" włącznie.
+// var myArray = ['uczę', 'się', 'programować', 'stop', 'lubię', 'to']
+//
+
+var stopArray = function(){
+  // TODO
+}
+
+// 12.
+// Pobieraj od użytkownika liczby tak długo, 
+// ż wpiszę liczbę większą niż 50. 
+// Wtedy wyświetl tablicę złożoną z wpisanych 
+// dotychczas liczb na stronie z ostatnią włącznie
+// 
+
+var including50 = function(){
+  // TODO
+}
+
+// 13.
+// Poproś użytkownika o wpisanie imienia i godziny. 
+// W alercie wyświetl użytkownikowi powitanie wraz z 
+// imieniem. Jeśli godzina wpisana przez użytkownika 
+// jest od 6 do 12, powitanie powinno brzmieć 
+// "dzień dobry", od 12 do 18 "Jak mija dzień?", 
+// od 18 do północy "Dobry wieczór", 
+// a od północy do 6 "idź spać!!!"
+//
+
+var nameAndHour = function(){
+  // TODO
+}
+
